2327b40686a60c3fcca6e8f8efdd490eff94cb08
Oz N Tiram <oz.tiram@gmail.com>
2017-10-12 22:14:49 +0200

    Make the example runable

--- examples/middlewares/logging_middleware.py
+++ examples/middlewares/logging_middleware.py
@@ -15,3 +15,11 @@ def log_environ(handler):
 
 app = log_environ(app)
 
+if __name__ == "__main__":
+    from wsgiref.simple_server import make_server
+    httpd = make_server('', 8000, app)
+    print("Serving on port 8000...")
+
+    # Serve until process is killed
+    httpd.serve_forever()
+
